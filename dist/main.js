6e801b3d83a27dcf3593759e7163ac503d3ed98f
dev-fatoh <fatohnasr360@gmail.com>
2023-01-29 10:48:59 +0200

    initial commit 2

--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,25 @@
 const btn = document.getElementById('btn');
-const items = document.querySelectorAll('li');
+const items = document.querySelectorAll('li.unread');
 const count = document.querySelector('.count');
 const redSign = document.querySelectorAll('.active');
 count.innerHTML = redSign.length;
 
-btn.addEventListener('click', () => {
+
+btn.addEventListener('click', updateNotification);
+
+//update read notification
+
+function updateNotification() {
   items.forEach((item) => {
     item.classList.add('read');
     item.classList.remove('unread');
+    count.innerHTML=0;
+    localStorage.setItem("name","fathi")
+
+    
+    redSign.forEach((y) => {
+      y.classList.remove('active');
+      y.classList.add('hidden');
+    });
   });
-  count.innerHTML = 0;
-  redSign.forEach((y) => {
-    y.classList.remove('active');
-    y.classList.add('hidden');
-  });
-});
+}
